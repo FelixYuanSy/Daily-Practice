0a4da5952e2a55352bd17c0dee18b85380d19d0d
FelixYuanSy <ysy12141314@gmail.com>
2025-10-01 10:53:44 +0800

    添加分离线程

--- StudyThread/TestThread.cc
+++ StudyThread/TestThread.cc
@@ -7,9 +7,10 @@
 void *routine(void *args)
 {
     sleep(1);
-    std::string name = static_cast<const char *>(args);
 
-    int cnt = 5;
+    std::string name = static_cast<const char *>(args);
+    delete (char *)args;
+    int cnt = 2;
     while (cnt--)
     {
         std::cout << "new线程名字" << name << std::endl;
@@ -28,21 +29,44 @@ int main()
         // char id[64];//这样写在新线程sleep完之后,主线程已经把id写完9了,新线程拿到的是同一个数组地址,所以都是9
         // snprintf(id, sizeof(id), "thread-%d", i);
         int n = pthread_create(&tid, NULL, routine, id);
-        if (n == 0)
+        if (n != 0)
+        {
+            delete[] id;
+            continue;
+        }
+        if (i >= 8)
+        {
+            pthread_detach(tid);
+        }
+        else
         {
             tids.push_back(tid);
         }
-        else
-            continue;
+        // if (n == 0)
+        // {
+        //     tids.push_back(tid);
+        // }
+        // else
+        //     continue;
     }
 
-    for (int i = 0; i < 10; i++)
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     int n = pthread_join(tids[i], nullptr);
+    //     if (n == 0)
+    //     {
+    //         std::cout << "等待线程成功" << std::endl;
+    //     }
+    //     else
+    //         std::cout << "不等了线程会自己退出" << std::endl;
+    //     continue;
+    // }
+
+    // 改成只需要需要join的
+    for (pthread_t thread : tids)
     {
-        int n = pthread_join(tids[i], nullptr);
-        if (n == 0)
-        {
-            std::cout << "等待线程成功" << std::endl;
-        }
+        pthread_join(thread, nullptr);
+        std::cout << "等待线程成功\n";
     }
     return 0;
 }
