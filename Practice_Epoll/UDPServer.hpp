34f51d6585e6b5212ac4e6ed61db7969f2d73e24
FelixYuanSy <ysy12141314@gmail.com>
2025-12-20 18:45:02 +0800

    server改用封装后的socket

--- Practice_Epoll/UDPServer.hpp
+++ Practice_Epoll/UDPServer.hpp
@@ -1,72 +1,57 @@
 #pragma once
+#include <cassert>
 #include <cerrno>
 #include <cstdint>
+#include <cstdio>
 #include <cstring>
+#include <functional>
 #include <iostream>
 #include <netinet/in.h>
 #include <strings.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include "InetAddr.hpp"
-
+#include "Udp_socket.hpp"
 const static uint16_t Default_Port = 8087;
 const static int Default_Fd = -1;
 const static int Default_Size = 1024;
+typedef std::function<void (const std::string &,std::string * resp)> Handler;
 class UdpServer {
 private:
-  int _socket_fd;
-  uint16_t _port;
+  UdpSocket _sock;
 
 public:
-  UdpServer(uint16_t port = Default_Port)
-      : _port(port), _socket_fd(Default_Fd) {}
-
-  void Init() {
-    // 初始化创建Socket
-    _socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (_socket_fd < 0) {
-      std::cout << errno << std::endl;
-      exit(errno);
-    }
-    std::cout << "Create UDP Socket Successed!\n";
-    // 设置addr_in属性，绑定socket
-    struct sockaddr_in local;
-    bzero(&local, sizeof(local));
-    local.sin_family = AF_INET;
-    local.sin_port = htons(_port);
-    local.sin_addr.s_addr = INADDR_ANY; // used to accept any incoming address，因为允许绑定具体ip所以让他自己分配
-
-    int ret = bind(_socket_fd, (sockaddr *)&local, sizeof(local));
-    if (ret != 0) {
-      std::cout << "UDP Server Bind Failed!\n";
-      std::cout << errno << std::endl;
-      exit(errno);
-    }
-    std::cout << "UDP Bind Successed!\n";
+  UdpServer()
+  {
+    assert(_sock.Socket());
+    
   }
-
-  /*服务器是一直运行的不退出
-      什么方法不退出一直保持循环？while？
-      用什么来接受数据 造buffer！defaultsize 为什么1024？
-
-  */
-  void start() {
-    char buffer[Default_Size];
-    while (1) {
-      // 接收数据放入buffer中
-      sockaddr_in peer;
-      socklen_t lens = sizeof(peer);
-      ssize_t recv_ret = recvfrom(_socket_fd, buffer, sizeof(buffer) - 1, 0,
-                                  (struct sockaddr *)&peer, &lens);
-      if (recv_ret > 0) {
-        //解析收到链接的ip和port
-        InetAddr addr(peer);
-        //刷新一下buffer
-        buffer[recv_ret]=0;
-        std::cout<<"["<<addr.PrintDebug()<<"]#" << buffer <<std::endl;
-        sendto(_socket_fd, buffer, strlen(buffer), 0, (struct sockaddr *)&peer, lens);
+  bool Start(std::string &ip,const uint16_t &port, Handler handler)
+  {
+    bool ret = _sock.Bind(ip, port);
+    if(!ret)
+    {
+      return false;
+    }
+    while(1)
+    {
+      std::string req;
+      std::string remote_ip;
+      uint16_t remote_port = 0;
+      bool ret = _sock.RecvFrom(&req,&remote_ip,&remote_port);
+      if(!ret)
+      {
+        continue;
       }
+      std::string resp;
+      handler(req,&resp);
+      _sock.SendTo(resp, remote_ip, remote_port);
+      printf("[%s : %d] req: %s, resp: %s\n",remote_ip.c_str(),remote_port,req.c_str(),resp.c_str());
+
     }
+    _sock.Close();
+    return true;
   }
+
   ~UdpServer(){}
 };
